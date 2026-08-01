0791f1289df745f66fb1fa3cf00ad334dd34c5a7
Yudhya Wicaksono <yudhyapatriaw@outlook.com>
2026-08-01 13:18:37 +0700

    First commit: Spring Initializr: Spring Web, Maven, Hibernate JPA

---
+++ src/test/java/com/yudhyapw/concert_book/ConcertBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yudhyapw.concert_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
